e5e511c2a792ce059fa4b6bf3751044ef5560de4
hdunne <tjo048@my.utsa.edu>
2015-10-15 19:08:55 -0500

    holly

---
+++ scripts/quiz.js
@@ -0,0 +1,87 @@
+function processQuiz3e()
+{
+	var q1 = document.forms.quiz1.question1;
+	var score = 0;
+	
+	if( q1[ 0 ].checked )
+		{
+			score = score + 1;
+		}
+	
+	var q2 = document.forms.quiz1.question2;
+	
+	if( q2[ 1 ].checked )
+		{
+			score = score + 1;
+		}
+	var q3 = document.forms.quiz1.question3;
+	
+	if( q3[ 2 ].checked )
+		{
+			score = score + 1;
+		}
+	var q4 = document.forms.quiz1.question3;
+	
+	if( q4[ 2 ].checked )
+		{
+			score = score + 1;
+		}
+	
+	alert( "You got " + score + " correct!");
+}
+
+function processQuizfinal()
+{
+	var q1 = document.forms.quiz2.question1;
+	var score = 0;
+	
+	if( q1[ 0 ].checked )
+		{
+			score = score + 1;
+		}
+	
+	var q2 = document.forms.quiz2.question2;
+	
+	if( q2[ 1 ].checked )
+		{
+			score = score + 1;
+		}
+	var q3 = document.forms.quiz2.question3;
+	
+	if( q3[ 2 ].checked )
+		{
+			score = score + 1;
+		}
+	var q4 = document.forms.quiz2.question3;
+	
+	if( q4[ 2 ].checked )
+		{
+			score = score + 1;
+		}
+	var q5 = document.forms.quiz2.question3;
+	
+	if( q5[ 2 ].checked )
+		{
+			score = score + 1;
+		}
+	var q6 = document.forms.quiz2.question3;
+	
+	if( q6[ 2 ].checked )
+		{
+			score = score + 1;
+		}
+	var q7 = document.forms.quiz2.question3;
+	
+	if( q7[ 2 ].checked )
+		{
+			score = score + 1;
+		}
+	var q8 = document.forms.quiz2.question3;
+	
+	if( q8[ 2 ].checked )
+		{
+			score = score + 1;
+		}
+	
+	alert( "You got " + score + " correct!");
+}
