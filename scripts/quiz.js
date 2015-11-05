3614d6b29b2366a834f8746cd685bfa2425eaf73
hdunne <tjo048@my.utsa.edu>
2015-11-05 16:29:53 -0600

    fixing functionality of final quiz

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -29,41 +29,45 @@ function processQuiz3e()
 
 function processQuizfinal()
 {
+
 	var q1 = document.forms.quiz2.question1;
 	var score = 0;
 	
-	if( q1[ 0 ].checked )
+	if( q1[ 2 ].checked )
 		{
 			score = score + 1;
 		}
 	
 	var q2 = document.forms.quiz2.question2;
 	
-	if( q2[ 1 ].checked )
+	if( q2[ 0 ].checked )
 		{
 			score = score + 1;
 		}
 	var q3 = document.forms.quiz2.question3;
 	
-	if( q3[ 2 ].checked )
+	if( q3[ 1 ].checked )
 		{
 			score = score + 1;
 		}
+
 	var q4 = document.forms.quiz2.question4;
-	
-	if( q4[ 3 ].checked )
+
+	if( q4[ 2 ].checked )
 		{
 			score = score + 1;
 		}
+
 	var q5 = document.forms.quiz2.question5;
 	
-	if( q5[ 4 ].checked )
+	if( q5[ 1 ].checked )
 		{
 			score = score + 1;
 		}
+
 	var q6 = document.forms.quiz2.question6;
 	
-	if( q6[ 5 ].checked )
+	if( q6[ 0 ].checked )
 		{
 			score = score + 1;
 		}
